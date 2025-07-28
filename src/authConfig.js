cee97a45dfc7c3090462857efff2192b50628893
Mirza Muhic <din-email@exempel.se>
2025-07-28 12:03:36 +0200

    Fixed authConfig import and updated Login.js for redirect

---
+++ src/authConfig.js
@@ -0,0 +1,11 @@
+export const msalConfig = {
+  auth: {
+    clientId: '431c04d-078b-4346-b028-34f510933b5a',
+    authority: 'https://login.microsoftonline.com/56474bc4-474a-4b51-aab1-1b7975f72b30',
+    redirectUri: 'https://hub.mirzamuhic.com',
+  },
+};
+
+export const loginRequest = {
+  scopes: ['User.Read'],
+};
