55d98d55203b3bebae083eb55e193273c395a9aa
Mirza Muhic <din-email@exempel.se>
2025-07-28 13:07:57 +0200

    Updated Login.js with imported images from old portal

--- src/views/auth/signIn/index.jsx
+++ src/views/auth/signIn/index.jsx
@@ -1,31 +1,79 @@
 import React from 'react';
-     import { Box, Flex, Text, Button, Image } from '@chakra-ui/react';
+     import { Box, Flex, Heading, Icon, Image, Text, VStack, Button } from '@chakra-ui/react';
+     import { FaMicrosoft } from 'react-icons/fa';
      import { useMsal } from '@azure/msal-react';
      import { msalConfig, loginRequest } from '../../../authConfig';
+     import BgImage from "../../../assets/img/Mirza-Omer.JPG"; // Bakgrundsbild
+     import Logo from "../../../assets/img/Stockholm-BDF-Gra-Liggande-2.png"; // Logotyp
 
-     const Login = () => {
+     const SignIn = () => {
        const { instance } = useMsal();
 
        const handleLogin = () => {
          instance.loginPopup(loginRequest).then(() => {
-           // Omdirigera till /admin/default efter inloggning
            window.location.href = 'https://hub.mirzamuhic.com/admin/default';
-         }).catch((error) => {
-           console.error(error);
+         }).catch(e => {
+           console.error("❌ Login misslyckades:", e);
          });
        };
 
        return (
-         <Flex height="100vh" width="100vw" align="center" justify="center">
-           <Box width="50%" p={12} bg="white" boxShadow="xl" borderRadius="md">
-             <Image src="/logo.png" alt="Stockholms Basketbollförbund" mb={8} maxH="180px" />
-             <Text fontSize="5xl" mb={6} fontWeight="bold">Logga in</Text>
-             <Text mb={8} fontSize="xl">Använd ditt Microsoft-konto för att komma åt portalen.</Text>
-             <Button colorScheme="blue" onClick={handleLogin} px={10} py={6} fontSize="lg">Logga in med Microsoft</Button>
-           </Box>
-           <Box width="50%" height="100%" backgroundImage="url('/background.jpg')" backgroundSize="cover" backgroundPosition="center" opacity="0.8" />
+         <Flex height="100vh" width="100vw">
+           {/* Left panel */}
+           <Flex
+             flex="1"
+             align="center"
+             justify="center"
+             bg="white"
+             direction="column"
+             p={10}
+           >
+             <Heading mb={4} fontSize="3xl" color="gray.800">Logga in</Heading>
+             <Text mb={8} color="gray.500">Använd ditt Microsoft-konto för att komma åt portalen.</Text>
+
+             <Button
+               leftIcon={<Icon as={FaMicrosoft} />}
+               bg="blue.600"
+               color="white"
+               size="lg"
+               w="80%"
+               _hover={{ bg: "blue.700" }}
+               onClick={handleLogin}
+             >
+               Logga in med Microsoft
+             </Button>
+
+             <Text mt={6} color="gray.400" fontSize="sm">
+               © {new Date().getFullYear()} Stockholms Basketbollförbund - Powered by Mirza Muhic
+             </Text>
+           </Flex>
+
+           {/* Right panel with background and logo */}
+           <Flex
+             flex="1"
+             bgGradient="linear(to-br, #1a202c, #2d3748)"
+             backgroundImage={`url(${BgImage})`}
+             backgroundSize="cover"
+             backgroundPosition="center"
+             p={10}
+             color="white"
+             direction="column"
+           >
+             <VStack spacing={6}>
+               <Image
+                 src={Logo}
+                 alt="Stockholm Basket"
+                 position="absolute"
+                 top="40px"
+                 left="40px"
+                 height="80px"
+                 width="auto"
+                 objectFit="contain"
+               />
+             </VStack>
+           </Flex>
          </Flex>
        );
      };
 
-     export default Login;
+     export default SignIn;
